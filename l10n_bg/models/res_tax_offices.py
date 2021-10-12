5828ee1b0191eb1a4fd1e3cf4a51f32df07d1d19
Rosen Vladimirov <vladimirov.rosen@gmail.cm>
2021-10-12 13:58:54 +0300

    added tools

--- l10n_bg/models/res_tax_offices.py
+++ l10n_bg/models/res_tax_offices.py
@@ -14,10 +14,35 @@ class TerritorialTaxOffices(models.Model):
     _inherits = {'res.partner': 'partner_id'}
 
     tax_code = fields.Char(string='NRA Office Code', help='The TAX Administration office code.')
-    partner_id = fields.Many2one('res.partner', string='NRA Partner', required=True, default=lambda self: self.env.ref('l10n_bg.nra'))
-    parent_id = fields.Many2one('res.territorial.tax.offices', string='Tax Territorial Directorates', required=True)
+    partner_id = fields.Many2one('res.partner', string='NRA Partner', required=True,
+                                 ondelete="cascade")
+    parent_tax_id = fields.Many2one('res.territorial.tax.offices', string='Tax Territorial Directorates', required=True)
     bank_ids = fields.One2many('res.partner.bank', 'partner_id', string='Banks',
                                domain="[('tax_office_id', '=', partner_id)]")
+
+    @api.model
+    def create(self, vals):
+        if not vals.get('name') or vals.get('partner_id'):
+            self.clear_caches()
+            return super(TerritorialTaxOffices, self).create(vals)
+        partner = self.env['res.partner'].create({
+            'name': vals['name'],
+            'is_company': True,
+            'customer': False,
+            'email': vals.get('email'),
+            'phone': vals.get('phone'),
+            'website': vals.get('website'),
+            'vat': vals.get('vat'),
+            'street': vals.get('street'),
+            'street2': vals.get('street2'),
+            'zip': vals.get('zip'),
+            'city': vals.get('city'),
+            'state_id': vals.get('state_id'),
+            'country_id': vals.get('country_id'),
+        })
+        vals['partner_id'] = partner.id
+        self.clear_caches()
+        return super(TerritorialTaxOffices, self).create(vals)
 
 
 class TerritorialDirectorates(models.Model):
@@ -26,7 +51,32 @@ class TerritorialDirectorates(models.Model):
     _inherits = {'res.partner': 'partner_id'}
 
     territorial_code = fields.Char(string='NRA territorial Code', help='NRA territorial Code from bank table.')
-    partner_id = fields.Many2one('res.partner', string='NRA Partner', required=True, default=lambda self: self.env.ref('l10n_bg.nra'))
-    child_ids = fields.One2many('res.territorial.tax.offices', 'parent_id', string='Tax offices')
+    partner_id = fields.Many2one('res.partner', string='NRA Partner', required=True,
+                                 ondelete="cascade")
+    child_ids = fields.One2many('res.territorial.tax.offices', 'parent_tax_id', string='Tax offices')
     bank_ids = fields.One2many('res.partner.bank', 'territorial_tax_id', string='Banks',
                                domain="[('partner_id', '=', partner_id)]")
+
+    @api.model
+    def create(self, vals):
+        if not vals.get('name') or vals.get('partner_id'):
+            self.clear_caches()
+            return super(TerritorialDirectorates, self).create(vals)
+        partner = self.env['res.partner'].create({
+            'name': vals['name'],
+            'is_company': True,
+            'customer': False,
+            'email': vals.get('email'),
+            'phone': vals.get('phone'),
+            'website': vals.get('website'),
+            'vat': vals.get('vat'),
+            'street': vals.get('street'),
+            'street2': vals.get('street2'),
+            'zip': vals.get('zip'),
+            'city': vals.get('city'),
+            'state_id': vals.get('state_id'),
+            'country_id': vals.get('country_id'),
+        })
+        vals['partner_id'] = partner.id
+        self.clear_caches()
+        return super(TerritorialDirectorates, self).create(vals)

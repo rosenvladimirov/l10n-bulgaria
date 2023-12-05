df0122dc101fea40cc7f10fe922a0920b40af93c
rosenvladimirov <rosen.vladimirov@mecproduction.eu>
2023-12-05 08:34:19 +0200

    Fix to 16.0.1.0.0

--- l10n_bg_vat_reports/__manifest__.py
+++ l10n_bg_vat_reports/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Bulgarian Accounting Reports',
-    'version': '1.0',
+    'version': '16.0.1.0.0',
     'category': 'Accounting/Localizations/Reporting',
     'summary': 'Reporting for Bulgarian Localization',
     'depends': [

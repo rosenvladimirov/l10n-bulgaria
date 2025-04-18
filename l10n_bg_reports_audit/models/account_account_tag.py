6cfdb3e65516a4a16f3ea9f0a6a10a3102a28378
rosenvladimirov <vladimirov.rosen@gmail.com>
2025-04-18 09:32:51 +0300

    Fix l10n_bg_applicability field selection method in tags
    Replaced the direct call to `get_l10n_bg_applicability()` with an internal method `_get_l10n_bg_applicability` for consistency and extensibility. Added `_get_l10n_bg_applicability` method to properly return the selection values. This change improves maintainability and aligns with best practices.

--- l10n_bg_reports_audit/models/account_account_tag.py
+++ l10n_bg_reports_audit/models/account_account_tag.py
@@ -9,11 +9,14 @@ class AccountAccountTag(models.Model):
     _name = "account.account.tag"
 
     l10n_bg_applicability = fields.Selection(
-        selection=get_l10n_bg_applicability(), string="Use for"
+        selection="_get_l10n_bg_applicability", string="Use for"
     )
     l10n_bg_code = fields.Char(
         "Code", compute="_compute_l10n_bg_code", help="A technical field for tag code"
     )
+
+    def _get_l10n_bg_applicability(self):
+        return get_l10n_bg_applicability(self)
 
     def _compute_l10n_bg_code(self):
         for record in self:

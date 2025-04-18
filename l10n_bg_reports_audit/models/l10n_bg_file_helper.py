155eba0bfd50be50f0988ec761717b7f369b2562
rosenvladimirov <vladimirov.rosen@gmail.com>
2025-04-18 09:49:15 +0300

    Refactor localization constants to remove translations.
    Replaced translation functions with plain text to simplify code and align with localization standards. This change reduces complexity and optimizes consistency in constant definitions.

--- l10n_bg_reports_audit/models/l10n_bg_file_helper.py
+++ l10n_bg_reports_audit/models/l10n_bg_file_helper.py
@@ -419,96 +419,90 @@ def get_l10n_bg_applicability(res):
 
 def get_doc_type():
     return [
-        ("01", _("Invoice")),
-        ("02", _("Debit note")),
-        ("03", _("Credit note")),
-        ("04", _("Storeable goods sent to EU")),
+        ("01", "Invoice"),
+        ("02", "Debit note"),
+        ("03", "Credit note"),
+        ("04", "Storeable goods sent to EU"),
         # Register of goods under the regime of storage of goods on demand,
         # sent or transported from the territory of the country to the territory of another member state
-        ("05", _("Storeable goods receive from EU")),
-        ("07", _("Customs declarations")),
+        ("05", "Storeable goods receive from EU"),
+        ("07", "Customs declarations"),
         # Customs declaration/customs document certifying completion of customs formalities
-        ("09", _("Protocols or other")),
-        ("11", _("Invoice - cash reporting")),
-        ("12", _("Debit notice - cash reporting")),
-        ("13", _("Credit notice - cash statement")),
-        ("50", _("Protocols fuel supplies")),
-        ("81", _("Sales report - tickets")),
+        ("09", "Protocols or other"),
+        ("11", "Invoice - cash reporting"),
+        ("12", "Debit notice - cash reporting"),
+        ("13", "Credit notice - cash statement"),
+        ("50", "Protocols fuel supplies"),
+        ("81", "Sales report - tickets"),
         (
             "82",
-            _("Special tax order"),
+            "Special tax order",
         ),  # Report on the sales made under a special tax order
-        ("83", _("sales of bread")),  # Report on the sales of bread
-        ("84", _("Sales of flour")),  # Report on the sales of flour
+        ("83", "sales of bread"),  # Report on the sales of bread
+        ("84", "Sales of flour"),  # Report on the sales of flour
         (
             "23",
-            _("Credit note art. 126b"),
+            "Credit note art. 126b",
         ),  # Credit notification under Art. 126b, para. 1 of VAT
         (
             "29",
-            _("Protocol under Art. 126b"),
+            "Protocol under Art. 126b",
         ),  # Protocol under Art. 126b, para. 2 and 7 of VAT
         (
             "91",
-            _("Protocol under Art. 151c"),
+            "Protocol under Art. 151c",
         ),  # Protocol for the required tax under Art. 151c, para. 3 of the law
-        ("92", _("Protocol under Art. 151g")),
+        ("92", "Protocol under Art. 151g"),
         # Protocol on the tax credit under Art. 151g,
         # para. 8 of the law or a report under Art. 104g, para. 14
-        ("93", _("Protocol under Art. 151c")),
+        ("93", "Protocol under Art. 151c"),
         # Protocol for the required tax under Art. 151c,
         # para. 7 of the law with a delivery recipient who does not apply the special regime
-        ("94", _("Protocol under Art. 151c, para. 7")),
+        ("94", "Protocol under Art. 151c, para. 7"),
         # Protocol for the required tax under Art. 151c,
         # para. 7 of the law with a delivery recipient, a person who applies the special regime
-        ("95", _("Protocol for free provision of foodstuffs")),
+        ("95", "Protocol for free provision of foodstuffs"),
         # Protocol for free provision of foodstuffs, to which Art. 6, para. 4, item 4 VAT
     ]
 
 
 def get_type_vat():
     return [
-        ("standard", _("Accounting document")),
-        ("117_protocol", _("Art. 117 - Protocols")),
-        ("119_report", _("Art. 119 - Report for sales")),
-        # ('120_sales_report', _('Art. 119 - Report for sales-special rules')),
-        # ('120_purchase_report', _('Art. 119 - Report for purchase-special rules')),
-        ("in_customs", _("Import Customs declaration")),
-        ("out_customs", _("Export Customs declaration")),
-        ("dropship", _("Dropship/Try party deal")),
+        ("standard", "Accounting document"),
+        ("117_protocol", "Art. 117 - Protocols"),
+        ("119_report", "Art. 119 - Report for sales"),
+        # ('120_sales_report', 'Art. 119 - Report for sales-special rules'),
+        # ('120_purchase_report', 'Art. 119 - Report for purchase-special rules'),
+        ("in_customs", "Import Customs declaration"),
+        ("out_customs", "Export Customs declaration"),
+        ("dropship", "Dropship/Try party deal"),
     ]
 
 
 def get_delivery_type():
     return [
-        ("01", _("Delivery under Part I of Annex 2 of VAT")),
-        ("02", _("Delivery under Part II of Annex 2 of VAT")),
-        ("03", _("Import under Annex 3 of VAT")),
-        ("07", _("Supply, import or IC acquisition of bread")),
-        ("08", _("Supply, import or IC acquisition of flour")),
+        ("01", "Delivery under Part I of Annex 2 of VAT"),
+        ("02", "Delivery under Part II of Annex 2 of VAT"),
+        ("03", "Import under Annex 3 of VAT"),
+        ("07", "Supply, import or IC acquisition of bread"),
+        ("08", "Supply, import or IC acquisition of flour"),
         (
             "51",
-            _(
                 "Arrival of goods on the territory of the country under "
-                "the regime of storage of goods until demand under Art. 15a of VAT"
-            ),
+                "the regime of storage of goods until demand under Art. 15a of VAT",
         ),
         (
             "53",
-            _(
                 "Replacement of the person for whom the goods were intended without "
-                "termination of the contract under Art. 15a, para. 4 of VAT"
-            ),
+                "termination of the contract under Art. 15a, para. 4 of VAT",
         ),
         (
             "54",
-            _("Marriage/absence/destruction of goods under Art. 15a, para. 10 of VAT"),
+            "Marriage/absence/destruction of goods under Art. 15a, para. 10 of VAT",
         ),
         (
             "58",
-            _(
-                "Termination of the contract under the mode of storage of goods until requested under Art. 15a of VAT"
-            ),
+                "Termination of the contract under the mode of storage of goods until requested under Art. 15a of VAT",
         ),
     ]
 

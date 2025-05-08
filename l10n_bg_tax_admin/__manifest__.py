51e314866000ce8f404399a9b59b818bdf475261
rosenvladimirov <vladimirov.rosen@gmail.com>
2025-05-08 13:33:51 +0300

    Add new Bulgarian tax administration module
    This module introduces functionalities required by Bulgarian legislation, including configuration, usage guides, and legal compliance support. Documentation and placeholders are provided to assist in configuring, using, and extending the module effectively.

---
+++ l10n_bg_tax_admin/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2025 Rosen Vladimirov
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'L10n Bg Tax Admin',
+    'summary': """This is a technical module that adds the necessary functionalities required by Bulgarian legislation.""",
+    'version': '18.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Rosen Vladimirov,Odoo Community Association (OCA)',
+    'depends': [
+        'account',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}

2c6a7b413428e25b85a7c9e59b5ad47c4f9d1888
rosenvladimirov <vladimirov.rosen@gmail.com>
2025-05-23 11:45:26 +0300

    Refactor Bulgarian localization configs and improve HTML theme
    Added OCA metadata, version, and compatibility details in l10n_bg_config manifest. Simplified and modernized the HTML structure for l10n_bg_report_theme, replacing outdated styles and content with responsive and user-friendly elements.

--- l10n_bg_config/__manifest__.py
+++ l10n_bg_config/__manifest__.py
@@ -11,7 +11,7 @@
     "development_status": 'Beta',
     "license": "AGPL-3",
     "author": "Rosen Vladimirov,Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/l10n-bulgaria",
+    "website": "https://github.com/rosenvladimirov/l10n-bulgaria",
     "depends": [
         "base",
         "account",

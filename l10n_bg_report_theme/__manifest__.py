2e3edc62cf5a0d1dc37efc733dfe94ec0e0090a1
Deyan Lyubenov <d.lyubenov@maylasoftware.com>
2025-08-12 15:18:21 +0300

    [UPD] Bump version to 18.0.3.0.4 in manifest for report theme sections

--- l10n_bg_report_theme/__manifest__.py
+++ l10n_bg_report_theme/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Report Theme Sections",
     "summary": """
         Report theme separate on sections.""",
-    "version": "18.0.3.0.3",
+    "version": "18.0.3.0.4",
     "license": "AGPL-3",
     "author": "Rosen Vladimirov,Odoo Community Association (OCA)",
     "website": "https://github.com/rosenvladimirov/l10n-bulgaria",

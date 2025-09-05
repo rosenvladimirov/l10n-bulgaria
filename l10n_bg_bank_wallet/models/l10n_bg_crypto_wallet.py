e09f8c687390fff0da39b53bbdf9fda754dc1c42
rosenvladimirov <vladimirov.rosen@gmail.com>
2025-09-05 13:56:40 +0300

    Lazy load `crypto_manager` and `filesystem_manager` in `l10n_bg_crypto_wallet` for improved initialization efficiency.

--- l10n_bg_bank_wallet/models/l10n_bg_crypto_wallet.py
+++ l10n_bg_bank_wallet/models/l10n_bg_crypto_wallet.py
@@ -168,10 +168,15 @@ class CryptoWallet(models.Model):
     master_password = fields.Char('Master password', store=False)
     decrypted_keys = fields.Text('Decrypted Keys', store=False, readonly=True)
 
-    def __init__(self, env, ids, prefetch_ids):
-        super().__init__(env, ids, prefetch_ids)
-        self._filesystem_manager = None
-        self._crypto_manager = CryptographyManager()
+    _crypto_manager = None
+    _filesystem_manager = None
+
+    @property
+    def crypto_manager(self):
+        """Lazy initialization of crypto manager"""
+        if not hasattr(self, '_crypto_manager') or not self._crypto_manager:
+            self._crypto_manager = CryptographyManager()
+        return self._crypto_manager
 
     @property
     def filesystem_manager(self):

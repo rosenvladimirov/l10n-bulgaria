93fc519fdd4fe756a47b074171c747264fb99068
rosenvladimirov <vladimirov.rosen@gmail.com>
2025-10-07 12:28:31 +0300

    Update module version and refactor multilanguage handling in `l10n_bg_config`
    Incremented the module version to `18.0.6.0.5`. Refactored multilanguage update logic in `_button_immediate_function` to improve clarity and ensure proper state handling.

--- l10n_bg_config/models/ir_module.py
+++ l10n_bg_config/models/ir_module.py
@@ -12,7 +12,9 @@ class Module(models.Model):
         res = super(Module, self)._button_immediate_function(function)
         if self:
             for module in self.filtered(lambda m: m.name in L10N_BG_MULTILANGUAGE):
+                is_l10n_bg_multilanguage = self.env.company.is_l10n_bg_multilanguage or {}
+                is_l10n_bg_multilanguage.update({module.name: module.state})
                 self.env.company.write(
-                    {'is_l10n_bg_multilanguage': self.env.company.is_l10n_bg_multilanguage.update(module.name, module.state)}
+                    {'is_l10n_bg_multilanguage': is_l10n_bg_multilanguage}
                 )
         return res

70103c2a16489e9bcb0f77e934ee498f5b866a6c
rosenvladimirov <vladimirov.rosen@gmail.com>
2025-10-16 07:51:45 +0300

    Add NCOP occupation classifications to `l10n_bg_payroll_classifications` with CSV data.

--- l10n_bg_payroll_classifications/__manifest__.py
+++ l10n_bg_payroll_classifications/__manifest__.py
@@ -1,7 +1,7 @@
 #  Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'Bulgarian HR Payroll Classifications',
-    'version': '18.0.3.0.1',
+    'version': '18.0.4.0.1',
     'category': 'Human Resources/Localization',
     'summary': 'Bulgarian localization for HR payroll with NKPD and Economic Activity classifications',
     'description': """

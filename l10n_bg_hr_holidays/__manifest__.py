5ce202db6287d223e06c9ffc5d2920f43d6adf58
rosenvladimirov <vladimirov.rosen@gmail.com>
2025-10-31 22:53:01 +0200

    Update `l10n_bg_hr_holidays`:
    - Modify access rights for `NSSI leave reason` to align with `HR Holidays Manager` group.
    - Refactor `hr.leave.type` views for better organization and usability.
    - Link `leave reason` to leave types in data records for improved tracking and consistency.
    - Update module version to 18.0.1.0.3 in the manifest.

--- l10n_bg_hr_holidays/__manifest__.py
+++ l10n_bg_hr_holidays/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Bulgaria - HR Holidays',
-    'version': '18.0.1.0.2',
+    'version': '18.0.1.0.3',
     'category': 'Human Resources/Time Off',
     'summary': 'Bulgarian localization for HR Holidays',
     'description': """

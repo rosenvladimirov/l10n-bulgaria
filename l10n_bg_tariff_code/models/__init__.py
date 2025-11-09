21a9b974721be48b4905445e6cd26f82d858f544
rosenvladimirov <vladimirov.rosen@gmail.com>
2025-11-09 14:20:30 +0200

    "Add tariff rate update and manual rate clearing logic to `product.product` in Bulgarian localization
    - Extend models to include `action_update_tariff_rate` and `action_clear_manual_tariff_rate` methods.
    - Enable manual and automatic tariff rate updates for product variants.
    - Integrate caching and real-time notification mechanisms."

--- l10n_bg_tariff_code/models/__init__.py
+++ l10n_bg_tariff_code/models/__init__.py
@@ -1,5 +1,6 @@
 from . import account_move_line
 from . import product_template
+from . import product_product
 from . import res_company
 from . import res_config_settings
 from . import l10n_bg_taric_cache

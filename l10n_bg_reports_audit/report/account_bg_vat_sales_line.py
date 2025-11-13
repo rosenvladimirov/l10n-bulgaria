afa1f5e66166efdbe58f5af8d0bb6f97f5c59e74
rosenvladimirov <vladimirov.rosen@gmail.com>
2025-11-13 09:10:10 +0200

    Update SQL queries and manifest version for localization fixes
    - Correct `REGEXP_REPLACE` syntax in SQL queries across multiple report files for consistency (`{{en_US}}` format).
    - Increment module version to `8.0.4` in the manifest to reflect updates.

--- l10n_bg_reports_audit/report/account_bg_vat_sales_line.py
+++ l10n_bg_reports_audit/report/account_bg_vat_sales_line.py
@@ -541,7 +541,7 @@ FROM {self._from()}
     LEFT JOIN account_account_tag_account_move_line_rel AS tag_line_rel
         ON tag_line_rel.account_move_line_id = aml.id
     LEFT JOIN (SELECT id,
-                    NULLIF(REGEXP_REPLACE(account_account_tag.name#>>'{en_US}', '\\D','','g'), '')::numeric AS tag_name,
+                    NULLIF(REGEXP_REPLACE(account_account_tag.name#>>'{{en_US}}', '\\D','','g'), '')::numeric AS tag_name,
                     {tax_negate},
                     l10n_bg_applicability
                     FROM account_account_tag

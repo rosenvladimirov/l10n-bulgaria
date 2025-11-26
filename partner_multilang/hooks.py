7173ef20d8aa0815dca1309e930e56b576fb5b95
rosenvladimirov <vladimirov.rosen@gmail.com>
2025-11-26 10:55:59 +0200

    Replace `cyrtranslit` dependency with `LANGUAGE_MAPPING` in `post_init_hook`
    - Updated `post_init_hook` to use `LANGUAGE_MAPPING` instead of `cyrtranslit` for language transliteration validation.
    - Adjusted imports to remove `cyrtranslit` and include `LANGUAGE_MAPPING`.

--- partner_multilang/hooks.py
+++ partner_multilang/hooks.py
@@ -1,9 +1,9 @@
 #  Part of Odoo. See LICENSE file for full copyright and licensing details.
 import logging
-import cyrtranslit
 
 from odoo import models
 from .models.res_transliterate import partner_name_translate
+from .models.res_transliterate import LANGUAGE_MAPPING
 
 from .odoo.models import regex_order
 
@@ -17,9 +17,8 @@ def pre_init_hook(env):
 
 
 def post_init_hook(env):
-    languages = cyrtranslit.supported()
     for lang in env['res.lang'].with_context(active_test=False).search([]):
-        if f"{lang.code[:2]}" in languages:
+        if f"{lang.code[:2]}" in LANGUAGE_MAPPING:
             lang.transliterate = True
 
     languages = env['res.lang'].search([('code', '!=', 'en_US'), ('transliterate', '=', True)])

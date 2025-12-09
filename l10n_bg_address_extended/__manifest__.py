6dbeb9742657371d2c582cbc81b0d6dc6c151a41
rosenvladimirov <vladimirov.rosen@gmail.com>
2025-12-09 13:55:11 +0200

    Bump module version to 19.0.1.0.1 in `l10n_bg_address_extended`

--- l10n_bg_address_extended/__manifest__.py
+++ l10n_bg_address_extended/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Bulgaria - Base address extended",
-    "version": "19.0.1.0.0",
+    "version": "19.0.1.0.1",
     "author": "Rosen Vladimirov",
     "category": "Localization",
     "website": "https://github.com/rosenvladimirov/l10n-bulgaria",

0321fad12c73d9e77dd56c847d169a1d1505a8e9
rosenvladimirov <vladimirov.rosen@gmail.com>
2025-12-30 16:30:45 +0200

    Add SCSS files for variable colors and fonts to optional layout bundle

--- l10n_bg_report_theme/__manifest__.py
+++ l10n_bg_report_theme/__manifest__.py
@@ -372,6 +372,8 @@ maintaining compliance with Bulgarian business document standards.
         ],
         # Опционален bundle - зарежда се само когато е нужен
         "l10n_bg_report_theme.optional_layout": [
+            "l10n_bg_report_theme/static/src/webclient/actions/reports/report_variable_colors.scss",
+            "l10n_bg_report_theme/static/src/webclient/actions/reports/report_variable_fonts.scss",
             "l10n_bg_report_theme/static/src/webclient/actions/reports/layout_assets/layout_background.scss",
             "l10n_bg_report_theme/static/src/webclient/actions/reports/layout_assets/layout_sections.scss"
         ],

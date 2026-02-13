bbb1d791e9a2e7c69057ece37e54c5c62f57c5d3
rosenvladimirov <vladimirov.rosen@gmail.com>
2026-02-14 00:20:03 +0200

    Simplify and standardize VAT report field names
    Streamlined field names in Bulgarian VAT purchase and sales reports to improve readability and consistency. Added the `model_report` parameter in the `_where` method to enhance reporting adaptability.

--- l10n_bg_reports_audit/report/account_bg_vat_purchases_line.py
+++ l10n_bg_reports_audit/report/account_bg_vat_purchases_line.py
@@ -328,7 +328,8 @@ class AccountBGCalcPurchasesLine(models.Model):
     def _where(self):
         if self.env.context.get("report_options"):
             date_from, date_to, tax_period, tax_periods, company_id, state = l10n_bg_where(
-                self.env, self.env.context.get("report_options")
+                self.env, self.env.context.get("report_options"),
+                model_report="purchase",
             )
             return f"""am.company_id = {company_id} AND am.state = ANY(ARRAY{state}) AND aat.l10n_bg_applicability = 'purchase' AND am.date >= '{date_from}' AND am.date <= '{date_to}'"""
         return """aat.l10n_bg_applicability = 'purchase'"""

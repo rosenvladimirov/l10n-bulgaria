2b838adbdb0333eac7a93ce298f9f00d4358f8ee
rosenvladimirov <vladimirov.rosen@gmail.com>
2026-02-23 10:25:08 +0200

    Refine docstrings for clarity in MT940 parser methods.

---
+++ l10n_bg_account_reconcile_patch/__init__.py
@@ -0,0 +1,3 @@
+from . import hooks
+
+post_load_hook = hooks.post_load_hook
---
+++ l10n_bg_account_reconcile_patch/__manifest__.py
@@ -0,0 +1,17 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Account Reconcile Partner Regex SQL Fix",
+    "description": """
+        Fix partner name regexp_matches for translated jsonb names.""",
+    "version": "18.0.1.0.0",
+    "license": "OPL-1",
+    "author": "Rosen Vladimirov",
+    "website": "https://github.com/rosenvladimirov/l10n-bulgaria-ee",
+    "depends": [
+        "account_reconcile_model_oca",
+    ],
+    "data": [],
+    "demo": [],
+    "post_load": "post_load_hook",
+}
